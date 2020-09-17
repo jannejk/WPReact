04bfd8f7bda850e24c719381649535848ce8cf52
Janne <janne@Jannes-MBP.Home>
2020-09-17 20:03:08 +0300

    Updates

---
+++ precache-manifest.ca3aa95b3a416faa2f95cce1d6af53d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a1a787b48a61b0374ac9a81ee4bc61",
+    "url": "/github.io/WPReact/index.html"
+  },
+  {
+    "revision": "aef8adecdefcb5b4f3f4",
+    "url": "/github.io/WPReact/static/css/main.1a480efa.chunk.css"
+  },
+  {
+    "revision": "6e396b3662ea2a52af5b",
+    "url": "/github.io/WPReact/static/js/2.df6e22bb.chunk.js"
+  },
+  {
+    "revision": "7eee473449a3578a3353723e7940e06d",
+    "url": "/github.io/WPReact/static/js/2.df6e22bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef8adecdefcb5b4f3f4",
+    "url": "/github.io/WPReact/static/js/main.f7920151.chunk.js"
+  },
+  {
+    "revision": "5151756c463176ccd9a8",
+    "url": "/github.io/WPReact/static/js/runtime-main.780d5f80.js"
+  }
+]);
